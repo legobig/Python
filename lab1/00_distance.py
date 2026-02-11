7fb4cd1cc49e7678448099592f4382f430519720
legobig <legobig228303@gmail.com>
2026-02-11 18:20:31 +0500

    Update 00_distance.py

--- lab1/00_distance.py
+++ lab1/00_distance.py
@@ -1,21 +1,17 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-# Есть словарь координат городов
-
 sites = {
     'Moscow': (550, 370),
     'London': (510, 510),
     'Paris': (480, 480),
 }
 
-# Составим словарь словарей расстояний между ними
-# расстояние на координатной сетке - ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+distances = {}
 
-
-# TODO здесь заполнение словаря
-distances={1,2,3}
-
+for city1 in sites:
+    for city2 in sites:
+        x1,y1=sites[city1]
+        x2,y2=sites[city2]
+        dist = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+        distances[f"{city1}-{city2}"]= round(dist, 2)
 print(distances)
 
 
