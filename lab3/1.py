bede5a817b5aa47b841d85cd66107a8ed444bfa3
legobig <legobig228303@gmail.com>
2026-03-04 18:18:14 +0500

    Update 1.py

--- lab3/1.py
+++ lab3/1.py
@@ -1,1 +1,9 @@
-
+def f(x):
+    total = 0
+    for i in x:
+        total+=1
+        print(i)
+        if isinstance(i, list):
+            total += f(i)
+    return total
+print (f([1, 2, [3, 4, [5]]]))
