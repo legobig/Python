eacf3bd7667905ab2f71709bcdb37babf2461ccc
legobig <legobig228303@gmail.com>
2026-03-05 15:18:25 +0500

    Update 2-rec.py

--- lab3/2-rec.py
+++ lab3/2-rec.py
@@ -4,6 +4,6 @@ def f(i):
     if i == 2:
         return -1/8
     
-    return ((i-1) * f(i-1)) / 3 + \
-           ((i-2) * f(i-2)) / 4
+    return ((i-1) * f(i-1)) / 3 + ((i-2) * f(i-2)) / 4
 print(f(3))
+
